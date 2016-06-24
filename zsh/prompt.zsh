cebff1e2a8ca8da2894ec638489024f780340c38
Idan Wender <idanwe2@gmail.com>
2016-06-24 18:14:21 +0300

    Change prompt style to be one line and remove user

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -32,7 +32,7 @@ directory="%F{blue}%~%f"
 
 # User and host {{{1
 user_and_host="%{\$fg_bold[black]%}$USERNAME at $(hostname)%f"
-user="%{\$fg_bold[black]%}$USERNAME"
+user="%{\$fg_bold[white]%}$USERNAME"
 
 # Git {{{1
 
@@ -47,14 +47,18 @@ add-zsh-hook precmd vcs_info
 zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:*' check-for-changes true # required to show staged/unstaged
 zstyle ':vcs_info:*' use-simple true # faster, but less accurate
-zstyle ':vcs_info:*' stagedstr ' %F{green}✗%f'
+zstyle ':vcs_info:*' stagedstr '%F{green}✗%f'
 zstyle ':vcs_info:*' unstagedstr ' %F{red}✗%f'
-zstyle ':vcs_info:*' formats ' at %F{green}%b%f%u%c'
-zstyle ':vcs_info:*' actionformats ' at %F{green}%b%f%u%c (%a)'
+zstyle ':vcs_info:*' formats '(%F{green}%b%f%u%c)'
+zstyle ':vcs_info:*' actionformats '%F{green}%b%f%u%c (%a)'
+# zstyle ':vcs_info:*' formats ' at %F{green}%b%f%u%c'
+# zstyle ':vcs_info:*' actionformats ' at %F{green}%b%f%u%c (%a)'
 
 git_status='${vcs_info_msg_0_}'
 
 # Full prompt {{{1
 
-PROMPT="$user $random_food $directory$git_status
-$exit_code"
+# PROMPT="$user $random_food $directory$git_status
+# $exit_code"
+
+PROMPT="$directory $random_food$git_status $exit_code"
